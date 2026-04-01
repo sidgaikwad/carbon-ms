f80b65d9567348ea4d1e77db20ea0c14562a6609
Brad Barbin <bradbarbin@protonmail.com>
2026-04-01 08:57:27 -0400

    build: update dependencies to remove critical and high vulnerabilities

--- apps/erp/app/polyfill.ts
+++ apps/erp/app/polyfill.ts
@@ -1,3 +1,12 @@
+if (typeof DOMMatrix === "undefined") {
+  // @ts-expect-error -- stub for SSR; d3-interpolate uses DOMMatrix for SVG transforms
+  globalThis.DOMMatrix = class DOMMatrix {
+    constructor() {
+      return { a: 1, b: 0, c: 0, d: 1, e: 0, f: 0 };
+    }
+  };
+}
+
 if (typeof Promise.withResolvers === "undefined") {
   Promise.withResolvers = <T>() => {
     let resolve!: (value: T | PromiseLike<T>) => void;
